2d3a87edee0873b817c94dcfb14c3092479de6b1
Bloqhub <oleg@bloqhub.io>
2022-09-15 12:07:05 +0300

    Skills         neuron: String a-z0-9         protocol: String a-z0-9 (multiindex)         endpoint: String a-z0-9         owner: String a-z0-9 (multiindex)         particle: Option<String> a-z0-9

---
+++ contracts/cw-cyb-skills/src/msg.rs
@@ -0,0 +1,58 @@
+use crate::state::{Entry};
+// use cosmwasm_std::{Uint64};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use cosmwasm_std::{
+   Addr,
+};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub owner: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum ExecuteMsg {
+    NewEntry {
+        neuron: String,
+        protocol: String,
+        endpoint: String,
+        particle: Option<String>,
+    },
+    UpdateEntry {
+        id: u64,
+        neuron: Option<String>,
+        protocol: Option<String>,
+        endpoint: Option<String>,
+        particle: Option<String>,
+    },
+    DeleteEntry {
+        id: u64,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    GetItems {
+        start_after: Option<u64>,
+        owner: Option<Addr>,
+        protocol: Option<String>,
+        limit: Option<u32>,
+    },
+}
+
+// Tokens
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct EntryResponse {
+    pub id: u64,
+    pub neuron: String,
+    pub protocol: String,
+    pub endpoint: String,
+    pub particle: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ListResponse {
+    pub entries: Vec<Entry>,
+}
